d74740dbe077f3bb28d1c383ded0b70530382a33
Alexis Daniel Matto <alexisdanielmatto@gmail.com>
2025-08-22 16:48:13 -0300

    Firts commit

---
+++ heroes/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+class Hero(models.Model):
+    codinome = models.CharField(max_length=50, unique=True)
+    nome_real = models.CharField(max_length=100, blank=True, null=True)
+    poder_principal = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=100)
+    historia = models.TextField(blank=True, null=True)
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.codinome
+
+
+# class seguindo(models.Models):
+#     seguindo = models.ForeignKey(Hero, on_delete=models.CASCADE)
+#     data = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return f"{self.seguindo.codinome}"
+
+# class seguidores(models.Models):
+#     seguidores = models.ForeignKey(Hero, on_delete=models.CASCADE)
+#     data = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return f"{self.seguidores.codinome}"
+
